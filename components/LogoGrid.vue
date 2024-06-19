61e49326e4cc3832c60e1aaa05eb8122dff3fd0f
taichi221228 <taichi221228@icloud.com>
2024-06-19 12:21:07 +0900

    feat: Update image size

--- components/LogoGrid.vue
+++ components/LogoGrid.vue
@@ -11,7 +11,7 @@ const items = ref(props.items);
 <template>
   <ul class="grid grid-cols-7 items-center gap-y-4">
     <li v-for="item in items" :key="item.path" class="p-0 m-0 list-none">
-      <img :src="item.path" :alt="item.name" class="w-18 mx-auto" />
+      <img :src="item.path" :alt="item.name" class="w-22 mx-auto" />
     </li>
   </ul>
 </template>
